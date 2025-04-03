6415867b78633a6c4dab56ad73e37abfe6a6ac29
Lars <lars@recap.dk>
2025-04-03 14:10:49 +0200

    small updates

---
+++ index.js
@@ -0,0 +1,62 @@
+let accountHolder = John
+
+const account = {
+    accountName : BertilSparkonto
+    balance : amountAccount (Math.Floor)
+}
+
+function getBalance () {
+//balance is a data typenumber
+}
+
+function atm() {
+    const message = parseFloat(
+        promt(
+            "Select a choice 1.) See Balance 2.) Make a deposit 3.) Make withdrawal 4.) Get account name 5.) Exit"
+        )
+    );
+}
+
+// after choice has been made, then what? User must type in a value if deposit/withdrawal. promt?
+
+    switch (message) {
+        case 1:
+            //kalla på funktioner utanför för de kan ikke se inn i scope
+            console.log("Your balance is" ${balance});
+            getBalance ();
+        case 2:
+            //add this amount to balance;
+            console.log("Thank you for your deposit. You now have");
+        case 3:
+            //lägg minus input till stored balance;
+        case 4:
+            //lägg pluss input till stored balance;
+        case 5:
+            clear();
+    }
+
+
+/*
+//perhaps use:------------
+break
+
+&& and
+|| or
+
+const variableName = 10; 
+
+isNaN(variableName); 
+
+// - getBalance, should be a function
+
+function getBalance () {
+
+}
+
+function withdrawal () {
+    subtract (input + balance)
+}
+
+function () {
+    add (input + balance)
+}
